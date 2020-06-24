f6929edfbfe04ebe911a1ff0752559045de58e49
Brayan Garcia <bgarcia.95@outlook.com>
2020-06-24 14:49:47 -0600

    Added productDetailScreen route

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,14 +1,17 @@
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
-import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
-import Colors from "../constants/Colors";
-import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
+import { Platform } from "react-native";
+
+import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
+import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
+import Colors from "../constants/Colors";
 
 const ProductsNavigator = createStackNavigator(
   {
     ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen,
   },
   {
     defaultNavigationOptions: {
