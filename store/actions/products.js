7b821fbad3f51bde4ba2c1e435f82aa330076a41
Brayan Garcia <bgarcia.95@outlook.com>
2020-07-05 23:02:21 -0600

    Adding Create and Update Actions

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,4 +1,28 @@
-import { DELETE_PRODUCT } from "../utils/actions";
+import {
+  CREATE_PRODUCT,
+  UPDATE_PRODUCT,
+  DELETE_PRODUCT,
+} from "../utils/actions";
+
+export const createProduct = (title, imageUrl, price, description) => ({
+  type: CREATE_PRODUCT,
+  product: {
+    title,
+    imageUrl,
+    price,
+    description,
+  },
+});
+
+export const updateProduct = (id, title, imageUrl, description) => ({
+  type: UPDATE_PRODUCT,
+  pId: id,
+  product: {
+    title,
+    imageUrl,
+    description,
+  },
+});
 
 export const deleteProduct = (productId) => ({
   type: DELETE_PRODUCT,
--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,5 +1,10 @@
 import PRODUCTS from "../../data/dummy-data";
-import { DELETE_PRODUCT } from "../utils/actions";
+import {
+  DELETE_PRODUCT,
+  CREATE_PRODUCT,
+  UPDATE_PRODUCT,
+} from "../utils/actions";
+import Product from "../../models/product";
 
 const initialState = {
   availableProducts: PRODUCTS,
@@ -8,6 +13,51 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case CREATE_PRODUCT:
+      const newProduct = new Product(
+        new Date().toString(),
+        "u1",
+        action.product.title,
+        action.product.imageUrl,
+        action.product.description,
+        action.product.price
+      );
+
+      return {
+        ...state,
+        availableProducts: state.availableProducts.concat(newProduct),
+        userProducts: state.userProducts.concat(newProduct),
+      };
+
+    case UPDATE_PRODUCT:
+      const productIndex = state.userProducts.findIndex(
+        (prod) => prod.id === action.pId
+      );
+      const updatedProduct = new Product(
+        action.pId,
+        state.userProducts[productIndex].ownerId,
+        action.product.title,
+        action.product.imageUrl,
+        action.product.description,
+        state.userProducts[productIndex].price
+      );
+
+      const updatedUserProducts = [...state.userProducts];
+      updatedUserProducts[productIndex] = updatedProduct;
+
+      const availableProductIndex = state.availableProducts.findIndex(
+        (prod) => prod.id === action.pId
+      );
+
+      const updatedAvailableProducts = [...state.availableProducts];
+      updatedAvailableProducts[availableProductIndex] = updatedProduct;
+
+      return {
+        ...state,
+        availableProducts: updatedAvailableProducts,
+        userProducts: updatedUserProducts,
+      };
+
     case DELETE_PRODUCT:
       return {
         ...state,
