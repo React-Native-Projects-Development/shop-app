37812f76b6ba8b1852d67c92101bd566a940d1d0
Brayan Garcia <bgarcia.95@outlook.com>
2020-07-08 03:02:03 -0600

    Displaying a spinner when sending an order to our server

--- screens/shop/CartScreen.jsx
+++ screens/shop/CartScreen.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { View, Text, Button, FlatList, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+} from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import Colors from "../../constants/Colors";
 import CartItem from "../../components/shop/CartItem";
@@ -8,7 +15,10 @@ import { removeFromCart } from "../../store/actions/cart";
 import { addOrder } from "../../store/actions/orders";
 
 const CartScreen = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
+
   const cartItems = useSelector((state) => {
     const transformedCartItems = [];
     for (const key in state.cart.items) {
@@ -24,7 +34,15 @@ const CartScreen = (props) => {
       a.productId > b.productId ? 1 : -1
     );
   });
+
   const dispatch = useDispatch();
+
+  const sendOrderHandler = async () => {
+    setIsLoading(true);
+    await dispatch(addOrder(cartItems, cartTotalAmount));
+    setIsLoading(false);
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.summary}>
@@ -35,12 +53,17 @@ const CartScreen = (props) => {
             $ {(Math.round(cartTotalAmount.toFixed(2)) * 100) / 100}
           </Text>
         </Text>
-        <Button
-          color={Colors.accentColor}
-          title="Order Now"
-          disabled={cartItems.length === 0}
-          onPress={() => dispatch(addOrder(cartItems, cartTotalAmount))}
-        />
+
+        {isLoading ? (
+          <ActivityIndicator size="small" color={Colors.primaryColor} />
+        ) : (
+          <Button
+            color={Colors.accentColor}
+            title="Order Now"
+            disabled={cartItems.length === 0}
+            onPress={sendOrderHandler}
+          />
+        )}
       </View>
       <FlatList
         data={cartItems}
