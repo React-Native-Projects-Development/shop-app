182c3109e1b4b5e26dda2b561165aae69865597c
Brayan Garcia <bgarcia.95@outlook.com>
2020-07-07 20:44:55 -0600

    Connecting to firebase

--- store/actions/products.js
+++ store/actions/products.js
@@ -2,17 +2,77 @@ import {
   CREATE_PRODUCT,
   UPDATE_PRODUCT,
   DELETE_PRODUCT,
+  SET_PRODUCTS,
 } from "../utils/actions";
+import Product from "../../models/product";
 
-export const createProduct = (title, imageUrl, price, description) => ({
-  type: CREATE_PRODUCT,
-  product: {
-    title,
-    imageUrl,
-    price,
-    description,
-  },
-});
+export const fetchProducts = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        "https://rn-shop-app-73043.firebaseio.com/products.json"
+      );
+
+      const resData = await response.json();
+
+      const loadedProducts = [];
+
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            "u1",
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price
+          )
+        );
+      }
+
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const createProduct = (title, imageUrl, price, description) => {
+  return async (dispatch) => {
+    // any async code you want!
+
+    try {
+      const response = await fetch(
+        "https://rn-shop-app-73043.firebaseio.com/products.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title,
+            description,
+            imageUrl,
+            price,
+          }),
+        }
+      );
+
+      const resData = await response.json();
+
+      dispatch({
+        type: CREATE_PRODUCT,
+        product: {
+          id: resData.name,
+          title,
+          imageUrl,
+          price,
+          description,
+        },
+      });
+    } catch (error) {}
+  };
+};
 
 export const updateProduct = (id, title, imageUrl, description) => ({
   type: UPDATE_PRODUCT,
