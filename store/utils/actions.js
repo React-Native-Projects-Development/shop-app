a50a1562ee9ca04f3ec131ee211e00144153825a
Brayan Garcia <bgarcia.95@outlook.com>
2020-07-08 16:08:27 -0600

    Removing unused action constants and adding AUTHENTICATE

--- store/utils/actions.js
+++ store/utils/actions.js
@@ -9,5 +9,4 @@ export const CREATE_PRODUCT = "CREATE_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
 
-export const SIGN_UP = "SIGN_UP";
-export const LOG_IN = "LOG_IN";
+export const AUTHENTICATE = "AUTHENTICATE";
