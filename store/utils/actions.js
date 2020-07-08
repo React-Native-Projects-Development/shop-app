00c982d06e4fe35f39d27e9f0792456ae83fdd6d
Brayan Garcia <bgarcia.95@outlook.com>
2020-07-07 20:44:48 -0600

    Adding SET_PRODUCTS constant

--- store/utils/actions.js
+++ store/utils/actions.js
@@ -2,6 +2,7 @@ export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 export const ADD_ORDER = "ADD_ORDER";
 
+export const SET_PRODUCTS = "SET_PRODUCTS";
 export const CREATE_PRODUCT = "CREATE_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
