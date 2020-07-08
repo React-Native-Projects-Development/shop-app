53ca65c84025b6f082604bfb186d4660a06b1352
Brayan Garcia <bgarcia.95@outlook.com>
2020-07-08 14:07:31 -0600

    Adding login action

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,7 +1,8 @@
-import { SIGN_UP } from "../utils/actions";
+import { SIGN_UP, LOG_IN } from "../utils/actions";
+
+const token = "AIzaSyCkGQ2Rmz9qes7ahh-qO8-dXLdvIl-xGcU";
 
 export const signup = (email, password) => {
-  const token = "AIzaSyCkGQ2Rmz9qes7ahh-qO8-dXLdvIl-xGcU";
   return async (dispatch) => {
     const response = await fetch(
       `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${token}
@@ -28,5 +29,34 @@ export const signup = (email, password) => {
     console.log(resData);
 
     dispatch({ type: SIGN_UP });
+  };
+};
+
+export const login = (email, password) => {
+  return async (dispatch) => {
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${token}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Something went wrong...");
+    }
+
+    const resData = await response.json();
+
+    console.log(resData);
+
+    dispatch({ type: LOG_IN });
   };
 };
