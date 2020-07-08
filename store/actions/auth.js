90b1856b638c0dcfc449157c84d134e011450171
Brayan Garcia <bgarcia.95@outlook.com>
2020-07-08 14:25:23 -0600

    Personalizing error messages

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -21,7 +21,14 @@ export const signup = (email, password) => {
     );
 
     if (!response.ok) {
-      throw new Error("Something went wrong...");
+      const errorResData = await response.json();
+      const errorId = errorResData.error.message;
+      let message = "Something went wrong!";
+      if (errorId === "EMAIL_EXISTS") {
+        message = "This email exists already!";
+      }
+
+      throw new Error(message);
     }
 
     const resData = await response.json();
@@ -50,7 +57,16 @@ export const login = (email, password) => {
     );
 
     if (!response.ok) {
-      throw new Error("Something went wrong...");
+      const errorResData = await response.json();
+      const errorId = errorResData.error.message;
+      let message = "Something went wrong!";
+      if (errorId === "EMAIL_NOT_FOUND") {
+        message = "Email not found!";
+      } else if (errorId === "INVALID_PASSWORD") {
+        message = "Password does not match!";
+      }
+
+      throw new Error(message);
     }
 
     const resData = await response.json();
